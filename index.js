23967750c0c392d4cd12b72e8453a99d6c72bc16
AbuKausar007 <a.25kausar@gmail.com>
2022-05-13 20:52:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//user: warehouseUser
+//pass: O8fST7bvNocdY0bv
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehouse.0cmkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("wareHouse").collection("inventory");
+
+    app.get("/inventories", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running warehouse server!");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
